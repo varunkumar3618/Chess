40cea82f5843de9d2cb5ad4495d46c7798c36a16
Travis Geis <ottobonn28@gmail.com>
2016-12-12 15:34:07 -0800

    Add batches of games

--- trainer.py
+++ trainer.py
@@ -16,24 +16,27 @@ def extractFeatureVector(board):
     return featureVector
 
 def main():
-    initialWeights = None
-    savedWeights = os.listdir("./training-weights/")
-    if len(savedWeights) > 0:
-        weightsName = sorted(savedWeights)[-1]
-        print "Loading ", weightsName
-        initialWeights = np.load("./training-weights/{}".format(weightsName))
+    numBatches = 100
+    for batchNum in xrange(numBatches):
+        print "Batch {} of {}".format(batchNum + 1, numBatches)
+        initialWeights = None
+        savedWeights = os.listdir("./training-weights/")
+        if len(savedWeights) > 0:
+            weightsName = sorted(savedWeights)[-1]
+            print "Loading ", weightsName
+            initialWeights = np.load("./training-weights/{}".format(weightsName))
 
-    tdAlg = TDLambda(featureExtractor=extractFeatureVector, initialWeights=initialWeights)
-    numGames=20
-    strongAgent = UCIChessAgent("Strong Stockfish", './engines/stockfish', 50)
-    weakAgent = UCIChessAgent("Weak Stockfish", './engines/stockfish', 10)
-    sim = Simulator(verbose=True, tdAlgorithm=tdAlg)
-    winners = sim.simulate(playerAgent=strongAgent, opponentAgent=weakAgent, numGames=numGames)
-    print "Winners: ", winners
-    weights = tdAlg.getWeights()
-    print weights
-    now = datetime.datetime.now()
-    np.save("./training-weights/{}-{} games".format(now, numGames), weights)
+        tdAlg = TDLambda(featureExtractor=extractFeatureVector, initialWeights=initialWeights)
+        numGames = 20
+        strongAgent = UCIChessAgent("Strong Stockfish", './engines/stockfish', 100)
+        weakAgent = UCIChessAgent("Weak Stockfish", './engines/stockfish', 10)
+        sim = Simulator(verbose=True, tdAlgorithm=tdAlg)
+        winners = sim.simulate(playerAgent=strongAgent, opponentAgent=weakAgent, numGames=numGames)
+        print "Winners: ", winners
+        weights = tdAlg.getWeights()
+        print weights
+        now = datetime.datetime.now()
+        np.save("./training-weights/{}-{} games".format(now, numGames), weights)
 
 if __name__ == "__main__":
     main()
